d53e466bfc670f9f33f17de04ba8e974590af15f
okajax <okajax@gmail.com>
2020-08-30 13:57:34 +0900

    add List test

---
+++ List.test.js
@@ -0,0 +1,27 @@
+import { mount } from '@vue/test-utils';
+import List from './List';
+
+describe('List', () => {
+  const wrapper = mount(List);
+
+  it('sort by asc', () => {
+    const expected = [
+      'apple',
+      'banana',
+      'cola'
+    ];
+    wrapper.vm.sort();
+    expect(wrapper.vm.$data.items).toBe(expected);
+  });
+
+  it('sort by desc', () => {
+    const expected = [
+      'cola',
+      'banana',
+      'apple'
+    ];
+    wrapper.vm.sort(true);
+    expect(wrapper.vm.$data.items).toBe(expected);
+  });
+
+});
