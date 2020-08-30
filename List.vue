5abf6b395233acc73412ece978a907f268d3571a
okajax <okajax@gmail.com>
2020-08-30 16:11:46 +0900

    add List

---
+++ List.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <ul>
+      <li
+        v-for="(item, i) in items"
+        :key="i"
+      >
+        {{ item }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      items: [
+        'cola',
+        'apple',
+        'banana'
+      ]
+    };
+  },
+  methods: {
+    sort(desc) {
+      this.items.sort((a, b) => {
+        let exp;
+        if (desc) {
+          exp = a > b;
+        } else {
+          exp = a < b;
+        }
+
+        return exp ? -1 : 1;
+      });
+    }
+  }
+};
+</script>
